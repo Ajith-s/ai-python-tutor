3c062259b0e6ffce02c805850d5cbfd2bac3c3c3
Ajith sharma <ajithsharma@Ajiths-iMac.local>
2025-06-13 19:43:00 -0700

    cleanup for streamlit deployment

--- firebase_config.py
+++ firebase_config.py
@@ -5,6 +5,7 @@ from firebase_admin import credentials, firestore
 
 def init_firebase():
     if not firebase_admin._apps:
+        # Initialize Firebase with your credentials
         firebase_config = st.secrets["firebase"]
         cred = credentials.Certificate(firebase_config)
         firebase_admin.initialize_app(cred)
